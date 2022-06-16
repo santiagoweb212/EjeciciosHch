6dc50c625eb5604fc7bbfdb1894caf87917bbcf8
santiagoweb212 <juanusca45@gmail.com>
2022-06-16 16:32:45 -0500

    ejercicios

---
+++ 04.js
@@ -0,0 +1,21 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function actividadesEnComun(persona1, persona2) {
+  // La funcion llamada 'actividadesEnComun' recibe como argumento dos arrays de actividades (strings) llamados 'persona1' y 'persona2'
+  // y debe devolver un array de strings con las actividades en comun ( aquellas que se repiten ) entre cada array.
+  // ej: persona1 = ['leer', 'comer', 'pasear', 'dormir', 'jugar']
+  //     persona2 = ['comer', 'dormir', 'futbol']
+  // actividadesEnComun(persona1, persona2) => ['comer', 'dormir']
+  // Tip: podes usar ciclos for anidados.
+  // Tu código aca:
+
+}
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = actividadesEnComun
---
+++ 05.js
@@ -0,0 +1,20 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function palabraMasLarga(array) {
+  // La funcion llamada 'palabraMasLarga' recibe un array 'array' de frases (strings) como parametro
+  // y debe devolver la palabra mas larga entre todas las frases ( Es decir la palabra con mayor cantidad de caracteres)
+  // Por ej:
+  // palabraMasLarga(['hola esto string', 'frase con palabra']) debe devolver 'palabra'
+
+  // Tu código aca:
+
+}
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = palabraMasLarga
